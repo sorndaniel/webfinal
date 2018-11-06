09dab07dba08aa076e944fe90d514a00d32e8b24
sorndaniel <sorndaniel@gmail.com>
2018-11-06 21:16:21 +0700

    add back to top & login menu icon

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function () {
+
+        // Show button after 100px
+        var showAfter = 100;
+        if ($(this).scrollTop() > showAfter) {
+            $('.back-to-top').fadeIn();
+        } else {
+            $('.back-to-top').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 52 }, 800);
+        return false;
+    });
+
+});
